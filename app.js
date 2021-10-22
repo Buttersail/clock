38119ebfcd9d091526ce41b5418c5688af987e09
Murat Memis <h.m.f.m.92@gmail.com>
2021-10-22 13:13:53 +0200

    add currentdate
    Added the current date to be shown in the app

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ setInterval(() => {
 
 function currentTime() {
   let time = new Date() //Read up and fully understand how this behaves/works
+  let dayName = time.getDay()
+  let month = time.getMonth()
+  let year = time.getFullYear()
+  let date = time.getDate()
   let hour = time.getHours()
   let min = time.getMinutes()
   let sec = time.getSeconds()
@@ -24,11 +28,17 @@ function currentTime() {
   //Current time
   let currentTime = hour + ':' + min + ':' + sec
 
-  //Current date, still working on this one
+  //Current date, weekdays starts on Sunday for some reason, haven't found out why.
+  //Temporary solution is to put the dayName - 1 and format the week accordingly
+  let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  let week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  let presentDay = week[dayName - 1] + ', ' + months[month] + ' ' + date + ', ' + year
 
   const clock = document.querySelector('#time')
+  const dayIntro = document.querySelector('#dayIntro')
 
   clock.innerHTML = currentTime
+  dayIntro.innerHTML = presentDay
 }
 
 currentTime()
